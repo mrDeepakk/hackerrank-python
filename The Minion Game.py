51c24e4e6cc8dccf7d2213a2e4f4be8fcdb7fa40
Deepak Kumar <deepak7589467031@gmail.com>
2022-10-02 12:39:42 +0530

    The Minion Game complete. count vowel/consonant

---
+++ The Minion Game.py
@@ -0,0 +1,20 @@
+import string
+def minion_game(s):
+    vowel=0
+    const=0
+    lis=list(s)
+    for i in range(len(lis)):
+        if lis[i] in "AEIOU":
+            vowel=vowel+(len(lis)-i)
+        else:
+            const=const+(len(lis)-i)
+    if const>vowel:
+        print("Stuart",const)
+    elif const==vowel:
+        print("Draw")
+    else:
+        print("Kevin",vowel)
+      
+if __name__ == '__main__':
+    s = input()
+    minion_game(s)
