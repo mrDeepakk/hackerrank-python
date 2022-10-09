a1ef041c590813ad84828e41398dfec4879d8e04
Deepak Kumar <deepak7589467031@gmail.com>
2022-10-09 20:36:02 +0530

    Introduction to Python

---
+++ Arithmetic Operators.py
@@ -0,0 +1,8 @@
+if __name__ == '__main__':
+    a = int(input())
+    b = int(input())
+    mul=a*b
+    sub=a-b
+    print(a+b)
+    print(sub)
+    print(mul)
---
+++ Write a function.py
@@ -0,0 +1,15 @@
+import calendar
+def is_leap(year):
+    leap = False
+    if(year % 4 == 0) :
+        if(year % 100 ==0) and (year % 400 ==0):
+            leap=True
+        elif(year % 100 ==0) or (year % 400 ==0):
+            leap=False
+        else:
+            leap=True
+    else :
+        leap=False
+    return leap
+year = int(input())
+print(is_leap(year))
